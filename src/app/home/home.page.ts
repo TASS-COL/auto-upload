c6c0e5e523774c920a29cccb2487da8c351be129
jmontoyag <148893220+jmontoyag@users.noreply.github.com>
2024-05-14 11:31:41 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Plugins } from '@capacitor/core';
+import { HttpClient } from '@angular/common/http';
+import { CapacitorUpdater, DownloadOptions  } from '@capgo/capacitor-updater';
+
+const { Intent } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    setInterval(()=>{
+      this.updateVersion();
+    }, 30000);
+  }
+
+  async updateVersion() {
+
+    let options: DownloadOptions = {url : "https://github.com/jpmontoya/securelogin/raw/main/dist.zip", version: "2.0.0"}
+
+    const version = await CapacitorUpdater.download(options);
+
+    await CapacitorUpdater.set(version);
+  }
+
+
+
 
 }
